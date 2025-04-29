6e4ddaa5e2e7656d612356570cf49fbfb3bbcbc5
Natanael Corpas Rivero <natel1988@gmail.com>
2025-04-29 17:12:39 +0200

    primera prueba

--- src/quiz.js
+++ src/quiz.js
@@ -2,6 +2,15 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions,
+        this.timeLimit = timeLimit,
+        this.timeRemaining = timeRemaining,
+        correctAnswers = 0,
+        currentQuestionIndex = 0
+
+
+    }
 
     // 2. getQuestion()
     
