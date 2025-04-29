dc84a1afbc4dfd48b40587db67c9092cf0231ec9
amalia barrigas <amaliabarrigas@gmail.com>
2025-04-29 19:49:52 +0200

    ejercicios terminados

--- src/quiz.js
+++ src/quiz.js
@@ -6,19 +6,65 @@ class Quiz {
         this.questions = questions,
         this.timeLimit = timeLimit,
         this.timeRemaining = timeRemaining,
-        correctAnswers = 0,
-        currentQuestionIndex = 0
-
+        this.correctAnswers = 0,
+        this.currentQuestionIndex = 0
 
     }
 
     // 2. getQuestion()
+    getQuestion () {
+        return this.questions[this.currentQuestionIndex];
+    }
+
     
     // 3. moveToNextQuestion()
 
+    moveToNextQuestion () {
+        this.currentQuestionIndex ++;
+    }
+
     // 4. shuffleQuestions()
+
+    shuffleQuestions () {
+        for (let i = this.questions.length - 1; i > 0; i-- ){
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }   
+
 
     // 5. checkAnswer(answer)
 
+    checkAnswer(answer){
+        const currentQuestion = this.questions[this.currentQuestionIndex];
+        if(currentQuestion.answer === answer){
+        this.correctAnswers++;
+        }
+        return answer;
+        }
     // 6. hasEnded()
+    hasEnded(){
+        if(this.currentQuestionIndex < this.questions.length){
+        return false;
+        }else if(this.currentQuestionIndex === this.questions.length){
+        return true;
+        }
+        }
+
+    //7. filterQuestionsByDifficulty() method
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty >= 1 && difficulty <= 3) {
+           this.questions = this.questions.filter(question => question.difficulty === difficulty);
+        }
+      }
+     
+
+    //8. averageDifficulty() method
+    averageDifficulty() {
+        let sumaDetodaslasdificultades = this.questions.reduce((acc, question) =>
+    { let nuevoacumulado = acc+ question.difficulty;
+        return nuevoacumulado
+    },0)
+    return sumaDetodaslasdificultades/this.questions.length
+    }
 }
