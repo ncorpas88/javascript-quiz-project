5c0d0496be294bf8ad09cd095afb012a07e18992
Natanael Corpas Rivero <natel1988@gmail.com>
2025-04-30 18:08:44 +0200

    tercer dia miniproyecto parte1

--- src/quiz.js
+++ src/quiz.js
@@ -1,70 +1,71 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor(questions, timeLimit, timeRemaining){
-        this.questions = questions,
-        this.timeLimit = timeLimit,
-        this.timeRemaining = timeRemaining,
-        this.correctAnswers = 0,
-        this.currentQuestionIndex = 0
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    (this.questions = questions),
+      (this.timeLimit = timeLimit),
+      (this.timeRemaining = timeRemaining),
+      (this.correctAnswers = 0),
+      (this.currentQuestionIndex = 0);
+  }
 
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  // 3. moveToNextQuestion()
+
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+
+  // 4. shuffleQuestions()
+
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
     }
+  }
 
-    // 2. getQuestion()
-    getQuestion () {
-        return this.questions[this.currentQuestionIndex];
+  // 5. checkAnswer(answer)
+
+  checkAnswer(answer) {
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+    if (currentQuestion.answer === answer) {
+      this.correctAnswers++;
     }
-
-    
-    // 3. moveToNextQuestion()
-
-    moveToNextQuestion () {
-        this.currentQuestionIndex ++;
+    return answer;
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
     }
+  }
 
-    // 4. shuffleQuestions()
-
-    shuffleQuestions () {
-        for (let i = this.questions.length - 1; i > 0; i-- ){
-            const j = Math.floor(Math.random() * (i + 1));
-            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
-        }
-    }   
-
-
-    // 5. checkAnswer(answer)
-
-    checkAnswer(answer){
-        const currentQuestion = this.questions[this.currentQuestionIndex];
-        if(currentQuestion.answer === answer){
-        this.correctAnswers++;
-        }
-        return answer;
-        }
-    // 6. hasEnded()
-    hasEnded(){
-        if(this.currentQuestionIndex < this.questions.length){
-        return false;
-        }else if(this.currentQuestionIndex === this.questions.length){
-        return true;
-        }
-        }
-
-    //7. filterQuestionsByDifficulty() method
-    filterQuestionsByDifficulty(difficulty) {
-        if (difficulty >= 1 && difficulty <= 3) {
-           this.questions = this.questions.filter(question => question.difficulty === difficulty);
-        }
-      }
-     
-
-    //8. averageDifficulty() method
-    averageDifficulty() {
-        let sumaDetodaslasdificultades = this.questions.reduce((acc, question) =>
-    { let nuevoacumulado = acc+ question.difficulty;
-        return nuevoacumulado
-    },0)
-    return sumaDetodaslasdificultades/this.questions.length
+  //7. filterQuestionsByDifficulty() method
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (question) => question.difficulty === difficulty
+      );
     }
+  }
+
+  //8. averageDifficulty() method
+  averageDifficulty() {
+    let sumaDetodaslasdificultades = this.questions.reduce((acc, question) => {
+      let nuevoacumulado = acc + question.difficulty;
+      return nuevoacumulado;
+    }, 0);
+    return sumaDetodaslasdificultades / this.questions.length;
+  }
 }
